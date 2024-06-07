8e97343274961024d95bc94c8cb751252e3e7516
tyson-tyler <benword25@gmail.com>
2024-06-07 12:32:48 +0530

    afasf

--- src/components/video/CommentSection/Comment.tsx
+++ src/components/video/CommentSection/Comment.tsx
@@ -3,6 +3,7 @@
 import Avatar from "@/components/Avatar";
 import { Channel, Comment as Come } from "@prisma/client";
 import dayjs from "@/vendor/devjs";
+import Link from "next/link";
 
 interface CommentProps {
   comment: Come & { channel: Channel };
@@ -10,18 +11,25 @@ interface CommentProps {
 const Comment: React.FC<CommentProps> = ({ comment }) => {
   return (
     <div key={comment.id} className="flex items-start gap-2">
-      <Avatar imageSrc={comment.channel.imageSrc} />
-      <div className="flex flex-col gap-1">
-        <div className="flex gap-2 items-center text-sm">
-          <p className="font-medium dark:text-white text-black">
-            @{comment.channel.handle}
-          </p>
+      <Link
+        href={`/channel/${comment.channel.id}`}
+        className="flex items-start gap-2"
+      >
+        <Avatar imageSrc={comment.channel.imageSrc} />
+        <div className="flex flex-col gap-1">
+          <div className="flex gap-2 items-center text-sm">
+            <p className="font-medium dark:text-white text-black">
+              @{comment.channel.handle}
+            </p>
+            <p className="font-light dark:text-white text-black">
+              {dayjs(comment.createdAt).fromNow()}
+            </p>
+          </div>
           <p className="font-light dark:text-white text-black">
-            {dayjs(comment.createdAt).fromNow()}
+            {comment.text}
           </p>
         </div>
-        <p className="font-light dark:text-white text-black">{comment.text}</p>
-      </div>
+      </Link>
     </div>
   );
 };
