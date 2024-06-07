46d66e9eccfad6c5033d84ab7ae4e903e44907dc
tyson-tyler <benword25@gmail.com>
2024-06-07 12:48:52 +0530

    afdsaf

--- src/components/Sketon.tsx
+++ src/components/Sketon.tsx
@@ -7,30 +7,6 @@ export function SkeletonCard() {
         <Skeleton className="relative w-full flex justify-center  md:h-[500px] lg:h-[550px] sm:h-[500px] h-[400px] aspect-video" />
         <Skeleton className="h-4 w-[200px]" />
       </div>
-      <div className="space-y-2">
-        <Skeleton className="relative w-full flex justify-center  md:h-[500px] lg:h-[550px] sm:h-[500px] h-[400px] aspect-video" />
-        <Skeleton className="h-4 w-[200px]" />
-      </div>
-      <div className="space-y-2">
-        <Skeleton className="relative w-full flex justify-center  md:h-[500px] lg:h-[550px] sm:h-[500px] h-[400px] aspect-video" />
-        <Skeleton className="h-4 w-[200px]" />
-      </div>
-      <div className="space-y-2">
-        <Skeleton className="relative w-full flex justify-center  md:h-[500px] lg:h-[550px] sm:h-[500px] h-[400px] aspect-video" />
-        <Skeleton className="h-4 w-[200px]" />
-      </div>
-      <div className="space-y-2">
-        <Skeleton className="relative w-full flex justify-center  md:h-[500px] lg:h-[550px] sm:h-[500px] h-[400px] aspect-video" />
-        <Skeleton className="h-4 w-[200px]" />
-      </div>
-      <div className="space-y-2">
-        <Skeleton className="relative w-full flex justify-center  md:h-[500px] lg:h-[550px] sm:h-[500px] h-[400px] aspect-video" />
-        <Skeleton className="h-4 w-[200px]" />
-      </div>
-      <div className="space-y-2">
-        <Skeleton className="relative w-full flex justify-center  md:h-[500px] lg:h-[550px] sm:h-[500px] h-[400px] aspect-video" />
-        <Skeleton className="h-4 w-[200px]" />
-      </div>
     </div>
   );
 }
