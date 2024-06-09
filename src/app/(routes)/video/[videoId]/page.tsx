350619b890ae627aae4efe346d572969a0fd0271
tyson-tyler <benword25@gmail.com>
2024-06-09 13:35:09 +0530

    fsfdsf

--- src/app/(routes)/video/[videoId]/page.tsx
+++ src/app/(routes)/video/[videoId]/page.tsx
@@ -60,7 +60,10 @@ export default async function VideoPage({
             <div className="flex justify-center w-full mt-4">
               <Dialog>
                 <DialogTrigger asChild>
-                  <Button variant="outline" className="lg:ml-[60px]">
+                  <Button
+                    variant="outline"
+                    className="md:ml-[60px] lg:ml-[60px]"
+                  >
                     <BiComment className="w-5 h-5 mr-2" />
                     Comment
                   </Button>
