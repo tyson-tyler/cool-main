a7a025cd8b620bea32f1a35e18db863825a46828
tyson-tyler <benword25@gmail.com>
2024-06-09 12:15:59 +0530

    dggdfg

--- src/app/(routes)/shorts/page.tsx
+++ src/app/(routes)/shorts/page.tsx
@@ -17,7 +17,7 @@ const Home = () => {
   const [trendingVideos, setTrendingVideos] = useState<VideoWithChannel[]>([]);
   const [subscriptions, setSubscriptions] = useState<Channel[]>([]);
   const [offset, setOffset] = useState(0);
-  const limit = 1; // Adjust this limit based on your preference
+  const limit = 3; // Adjust this limit based on your preference
   const [loading, setLoading] = useState(true);
   const [hasMore, setHasMore] = useState(true);
   const observer = useRef<IntersectionObserver>();
