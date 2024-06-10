f99453eb8e0476ea4c0fdd00d13cf4adb9beee84
tyson-tyler <benword25@gmail.com>
2024-06-10 16:58:23 +0530

    gsfdgfd

--- src/components/video/VideoPlayer.tsx
+++ src/components/video/VideoPlayer.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect } from "react";
 import MuxPlayer from "@mux/mux-player-react";
-import { CldVideoPlayer } from "next-cloudinary";
 
 interface VideoPlayerProps {
   videoSrc: string;
@@ -10,35 +9,17 @@ interface VideoPlayerProps {
 }
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoSrc, userId }) => {
-  useEffect(() => {
-    const recordWatchHistory = async () => {
-      try {
-        await fetch("/api/history/record", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ userId, videoId: videoSrc }),
-        });
-      } catch (error) {
-        console.error("Failed to record watch history:", error);
-      }
-    };
-
-    recordWatchHistory();
-  }, [videoSrc, userId]);
-
   return (
     <div className="relative w-full flex justify-center m-auto group dark:bg-black">
       <div className="dark:text-white text-black z-40 w-full">
-        <CldVideoPlayer
+        <MuxPlayer
           playback-id={videoSrc}
           src={videoSrc}
           autoPlay
+          accentColor="#ea580c"
+          metadata-video-title="Test VOD"
+          metadata-viewer-user-id={userId}
           className="lg:h-[600px] md:h-[500px] sm:h-[400px] h-[280px] w-full rounded-md"
-          sourceTypes={["hls", "dash"]}
-          transformation={{ streaming_profile: "hd" }}
-          aspectRatio="fill"
         />
       </div>
     </div>
