31772f7877a85f9c47d0dd24ff527f63434bbf6c
tyson-tyler <benword25@gmail.com>
2024-06-11 13:25:30 +0530

    asdfsaf

--- src/components/Sketon.tsx
+++ src/components/Sketon.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export function SkeletonCard() {
   return (
-    <div className="grid-container space-y-3">
+    <div className="flex flex-wrap justify-center space-y-3">
       <div className="space-y-2">
         <Skeleton className="relative w-full flex justify-center  md:h-[500px] lg:h-[550px] sm:h-[500px] h-[400px] aspect-video" />
         <Skeleton className="h-4 w-[200px]" />
