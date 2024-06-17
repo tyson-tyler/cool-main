47fd6b21fe4115e925a8b2022b49afbf08e6e692
tyson-tyler <benword25@gmail.com>
2024-06-17 22:59:03 +0530

    fadsf

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,124 +1,69 @@
 "use client";
 import React, { useContext, useEffect } from "react";
-import Link from "next/link";
-
-import { CurrentChannelContext } from "@/context/CreateChannelContext";
 import { useRouter } from "next/navigation";
+import { CurrentChannelContext } from "@/context/CreateChannelContext";
 import { CreateChannelModalContext } from "@/context/CreateChannelModelContext";
 import { SiHomeadvisor } from "react-icons/si";
 import { BiSolidCloudUpload, BiSolidParty } from "react-icons/bi";
 import { GiFilmSpool } from "react-icons/gi";
 import { RiAccountPinBoxFill } from "react-icons/ri";
-import { FaPaintBrush } from "react-icons/fa";
-import { FaGrinHearts } from "react-icons/fa";
+import { FaPaintBrush, FaGrinHearts } from "react-icons/fa";
 
 const Footer = () => {
   const createChannelModal = useContext(CreateChannelModalContext);
   const router = useRouter();
-
   const currentChannel = useContext(CurrentChannelContext);
+
   useEffect(() => {
-    // Prefetch the about page on component mount
-    // router.prefetch("/about");
+    // Prefetch pages for better performance
+    // router.prefetch("/");
     // router.prefetch("/creator");
     // router.prefetch("/shorts");
-    // router.prefetch("/");
-    // router.prefetch(`/channel/${currentChannel?.id}`);
     // router.prefetch("/studio");
     // router.prefetch("/studio/upload");
-  }, [router]);
+    // if (currentChannel) {
+    //   router.prefetch(`/channel/${currentChannel.id}`);
+    // }
+  }, [router, currentChannel]);
+
+  const menuItems = [
+    { icon: <SiHomeadvisor />, path: "/" },
+    { icon: <BiSolidParty />, path: "/creator" },
+    { icon: <GiFilmSpool />, path: "/shorts" },
+    {
+      icon: <BiSolidCloudUpload />,
+      path: "/studio/upload",
+      requiresChannel: true,
+    },
+    {
+      icon: <RiAccountPinBoxFill />,
+      path: currentChannel ? `/channel/${currentChannel.id}` : "",
+      requiresChannel: true,
+    },
+    { icon: <FaGrinHearts />, path: "/like", requiresChannel: true },
+    { icon: <FaPaintBrush />, path: "/studio", requiresChannel: true },
+  ];
+
+  const handleItemClick = (path: any, requiresChannel = false) => {
+    if (requiresChannel && !currentChannel) {
+      createChannelModal?.onOpen();
+    } else {
+      router.push(path);
+    }
+  };
 
   return (
-    <div
-      className={`fixed bottom-0 flex justify-center p-3 w-full items-center container z-10 bg-gray-200 shadow-md dark:bg-neutral-900 md:hidden mt-10 h-16`}
-    >
-      <div className="flex justify-between md:hidden">
-        <div className="flex gap-5 ml-3">
+    <div className="fixed bottom-[-1px] w-full bg-gray-200 shadow-md dark:bg-neutral-900 z-10 md:hidden">
+      <div className="flex justify-around p-3">
+        {menuItems.map((item, index) => (
           <div
-            onClick={() => {
-              router.push("/");
-            }}
-            className="flex flex-row  cursor-pointer items-center gap-x-3 text-2xl opacity-80 hover:scale-105 transform transition gap-6"
+            key={index}
+            onClick={() => handleItemClick(item.path, item.requiresChannel)}
+            className="cursor-pointer text-2xl opacity-80 hover:opacity-100 transform transition hover:scale-110"
           >
-            <div className="flex items-center gap-x-3 text-2xl my-5 opacity-80 hover:opacity-100 gap-6">
-              <SiHomeadvisor className="w-7 h-7" />
-            </div>
+            {item.icon}
           </div>
-          <div
-            onClick={() => {
-              router.push("/creator");
-            }}
-            className="flex flex-row  cursor-pointer items-center gap-x-3 text-2xl opacity-80 hover:scale-105 transform transition gap-6"
-          >
-            <div className="flex items-center gap-x-3 text-2xl my-5 opacity-80 hover:opacity-100 gap-6">
-              <BiSolidParty className="w-7 h-7" />
-            </div>
-          </div>
-          <div
-            onClick={() => {
-              router.push("/shorts");
-            }}
-            className="flex flex-row  cursor-pointer items-center gap-x-3 text-2xl opacity-80 hover:scale-105 transform transition gap-6"
-          >
-            <div className="flex items-center gap-x-3 text-2xl my-5 opacity-80 hover:opacity-100 gap-6">
-              <GiFilmSpool className="w-7 h-7" />
-            </div>
-          </div>
-          <div
-            onClick={() => {
-              if (!currentChannel) createChannelModal?.onOpen();
-              else router.push("/studio/upload");
-            }}
-            className="flex flex-row  cursor-pointer items-center gap-x-3 text-2xl opacity-80 hover:scale-105 transform transition gap-6"
-          >
-            <div className="flex items-center gap-x-3 text-2xl my-5 opacity-80 hover:opacity-100 gap-6">
-              <BiSolidCloudUpload className="w-7 h-7" />
-            </div>
-          </div>
-
-          <div
-            onClick={() => {
-              if (!currentChannel) {
-                createChannelModal?.onOpen();
-              } else {
-                router.push(`/channel/${currentChannel.id}`);
-              }
-            }}
-            className="flex flex-row  cursor-pointer items-center gap-x-3 text-2xl opacity-80 hover:scale-105 transform transition gap-6"
-          >
-            <div className="flex items-center gap-x-3 text-2xl my-5 opacity-80 hover:opacity-100 gap-6">
-              <RiAccountPinBoxFill className="w-7 h-7" />
-            </div>
-          </div>
-          <div
-            onClick={() => {
-              if (!currentChannel) {
-                createChannelModal?.onOpen();
-              } else {
-                router.push(`/like`);
-              }
-            }}
-            className="flex flex-row justify-center  lg:justify-normal  cursor-pointer items-center gap-x-3 text-2xl opacity-80 hover:scale-105 transform transition gap-6"
-          >
-            <div className="flex items-center gap-x-3 text-2xl my-5 opacity-80 hover:opacity-100 gap-6">
-              <FaGrinHearts className="w-7 h-7" />
-            </div>
-          </div>
-          <div
-            onClick={() => {
-              if (!currentChannel) {
-                createChannelModal?.onOpen();
-              } else {
-                router.push(`/studio`);
-              }
-            }}
-            className="flex flex-row  cursor-pointer items-center gap-x-3 text-2xl opacity-80 hover:scale-105 transform transition gap-6"
-          >
-            <div className="flex items-center gap-x-3 text-2xl my-5 opacity-80 hover:opacity-100 gap-6">
-              <FaPaintBrush className="w-7 h-7" />
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
