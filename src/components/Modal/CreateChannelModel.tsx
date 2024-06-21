f54e5848e9eb72724c6cab8377b1a98212efc217
tyson-tyler <benword25@gmail.com>
2024-06-21 10:14:51 +0530

    sdfsf

--- src/components/Modal/CreateChannelModel.tsx
+++ src/components/Modal/CreateChannelModel.tsx
@@ -1,14 +1,14 @@
-"use client";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import Avatar, { AvatarSize } from "../Avatar";
-import { Button } from "../ui/button";
 import { useContext, useState } from "react";
-import Input from "../shared/Input";
 import axios from "axios";
-import MediaUpload from "../PhotoUpload";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { CreateChannelModalContext } from "@/context/CreateChannelModelContext";
+import Input from "../shared/Input";
+import MediaUpload from "../PhotoUpload";
+import Avatar, { AvatarSize } from "../Avatar";
+import { Button } from "../ui/button";
+import { Loader } from "lucide-react";
 
 const CreateChannelModel = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -39,19 +39,21 @@ const CreateChannelModel = () => {
   };
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    setIsLoading(true);
+    setIsLoading(true); // Start loading indicator
+
     axios
       .post("/api/channels", data)
       .then(() => {
-        toast.success("Account Ready");
+        toast.success("Channel created successfully");
         createChannelModal?.onClose();
         router.refresh();
       })
-      .catch(() => {
-        toast.error("Something went wrong");
+      .catch((error) => {
+        console.error("Error creating channel:", error);
+        toast.error("Failed to create channel. Please try again later.");
       })
       .finally(() => {
-        setIsLoading(false);
+        setIsLoading(false); // Stop loading indicator
       });
   };
 
@@ -61,7 +63,7 @@ const CreateChannelModel = () => {
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center z-50 dark:bg-gray-900 bg-white text-black dark:text-white shadow-md w-full lg:w-1/2 rounded-xl">
           <div className="flex justify-center items-center border-b p-3 border-gray-500">
-            <h2 className="text-3xl font-bold usespan">Welcome here</h2>
+            <h2 className="text-3xl font-bold usespan">Create Channel</h2>
           </div>
           <div className="flex flex-col items-center py-4 gap-5">
             <Avatar size={AvatarSize.large} imageSrc={imageSrc} />
@@ -80,8 +82,8 @@ const CreateChannelModel = () => {
                 value: /^[a-zA-Z0-9 ]*$/,
                 message: "Invalid name format",
               }}
-              required // Adding required attribute to make the input required
-              className={`w-3/4 mb-3 ${errors.name ? "border-red-500" : ""}`} // Applying conditional border color based on errors
+              required
+              className={`w-3/4 mb-3 ${errors.name ? "border-red-500" : ""}`}
             />
             <Input
               id="handle"
@@ -89,8 +91,8 @@ const CreateChannelModel = () => {
               disabled={isLoading}
               register={register}
               errors={errors}
-              required // Adding required attribute to make the input required
-              className={`w-3/4 ${errors.handle ? "border-red-500" : ""}`} // Applying conditional border color based on errors
+              required
+              className={`w-3/4 ${errors.handle ? "border-red-500" : ""}`}
             />
           </div>
           <div className="p-2 border-t border-gray-600 flex justify-end gap-3">
@@ -101,8 +103,19 @@ const CreateChannelModel = () => {
             >
               Cancel
             </Button>
-            <Button className="p-2" onClick={handleSubmit(onSubmit)}>
-              Create Channel
+            <Button
+              className="p-2"
+              onClick={handleSubmit(onSubmit)}
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <div className="flex">
+                  <Loader className="w-5 h-5 animate-spin mr-2" />
+                  Creating ...
+                </div>
+              ) : (
+                "Create Channel"
+              )}
             </Button>
           </div>
         </div>
