73016ce9dc6864d0e41a68560cf5e6152cd7ea2f
tyson-tyler <benword25@gmail.com>
2024-06-30 19:35:50 +0530

    dfsa

--- src/app/(routes)/channel/[channelId]/page.tsx
+++ src/app/(routes)/channel/[channelId]/page.tsx
@@ -25,7 +25,7 @@ export default async function ChannelPage({
   return channel ? (
     <>
       <div className="flex">
-        <div className="sm:hidden md:flex flex flex-between md:mr-0 mt-12 md:ml-5 lg:ml-0">
+        <div className="sm:hidden md:flex flex flex-between md:mr-0 mt-12  lg:ml-0">
           <LeftBar subscribedChannels={subscriptions} />
         </div>
 
