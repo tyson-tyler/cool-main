56cfb06ecd7364d2a09b6760d41e40fe3e2a186a
tyson-tyler <benword25@gmail.com>
2024-06-30 19:28:25 +0530

    afds

--- src/components/second.tsx
+++ src/components/second.tsx
@@ -111,6 +111,10 @@ const LeftBar: React.FC<SideBarProps> = ({ subscribedChannels }) => {
               >
                 <div className="flex items-center gap-7 text-2xl">
                   {item.icon}
+
+                  <span className="hidden lg:block text-lg font-semibold">
+                    {item.name}
+                  </span>
                 </div>
               </div>
             )
