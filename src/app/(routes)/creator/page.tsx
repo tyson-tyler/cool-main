1e7205b3467bc5c48f986ab2cc0da9f31156a6ff
tyson-tyler <benword25@gmail.com>
2024-07-01 19:51:01 +0530

    fdas

--- src/app/(routes)/creator/page.tsx
+++ src/app/(routes)/creator/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import {
   useEffect,
   useState,
@@ -7,13 +8,12 @@ import {
   lazy,
   Suspense,
 } from "react";
+import useSWR from "swr"; // Import SWR for data fetching
+import axios from "axios";
 import { Channel, Video } from "@prisma/client";
 import { SkeletonCard } from "@/components/Sketon";
-import Loader from "@/components/Loader";
-import axios from "axios"; // Import Axios for making HTTP requests
 import { SkeletonDemo } from "@/components/shared/Trop";
 
-// Lazy load components
 const LeftBar = lazy(() => import("@/components/Leftbar"));
 const VideoCard = lazy(() => import("@/components/shared/VideoCard"));
 
@@ -21,16 +21,41 @@ interface VideoWithChannel extends Video {
   channel: Channel;
 }
 
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+
 const Home = () => {
   const [trendingVideos, setTrendingVideos] = useState<VideoWithChannel[]>([]);
   const [subscriptions, setSubscriptions] = useState<Channel[]>([]);
   const [offset, setOffset] = useState(0);
-  const limit = 2; // Adjust this limit based on your preference
+  const limit = 10;
   const [loading, setLoading] = useState(true);
   const [hasMore, setHasMore] = useState(true);
   const observer = useRef<IntersectionObserver>();
 
-  const fetchTrendingVideos = async (offset: number, limit: number) => {
+  const { data: initialVideos, error: videosError } = useSWR(
+    `/api/hello?offset=0&limit=${limit}`,
+    fetcher
+  );
+  const { data: initialSubscriptions, error: subscriptionsError } = useSWR(
+    "/api/sub",
+    fetcher
+  );
+
+  useEffect(() => {
+    if (initialVideos) {
+      setTrendingVideos(initialVideos);
+      setHasMore(initialVideos.length === limit);
+      setLoading(false);
+    }
+  }, [initialVideos]);
+
+  useEffect(() => {
+    if (initialSubscriptions) {
+      setSubscriptions(initialSubscriptions);
+    }
+  }, [initialSubscriptions]);
+
+  const fetchMoreVideos = useCallback(async () => {
     try {
       const response = await axios.get(
         `/api/pure?offset=${offset}&limit=${limit}`
@@ -39,37 +64,22 @@ const Home = () => {
       setTrendingVideos((prevVideos) => [...prevVideos, ...videos]);
       setHasMore(videos.length === limit);
     } catch (error) {
-      console.error("Failed to fetch trending videos", error);
+      console.error("Failed to fetch more videos", error);
     } finally {
       setLoading(false);
     }
-  };
-
-  const fetchSubscriptions = async () => {
-    try {
-      const response = await axios.get("/api/sub"); // Adjust the endpoint if necessary
-      const subs = response.data;
-      setSubscriptions(subs);
-    } catch (error) {
-      console.error("Failed to fetch subscriptions", error);
-    }
-  };
-
-  useEffect(() => {
-    setLoading(true);
-    fetchTrendingVideos(0, limit); // Initial fetch
-    fetchSubscriptions();
-  }, []);
-
-  useEffect(() => {
-    if (offset === 0) return;
-    setLoading(true);
-    fetchTrendingVideos(offset, limit); // Fetch more videos when offset changes
   }, [offset]);
+
+  useEffect(() => {
+    if (offset > 0) {
+      setLoading(true);
+      fetchMoreVideos();
+    }
+  }, [offset, fetchMoreVideos]);
 
   const lastVideoElementRef = useCallback(
     (node: any) => {
-      if (loading || !hasMore) return;
+      if (loading) return;
       if (observer.current) observer.current.disconnect();
       observer.current = new IntersectionObserver((entries) => {
         if (entries[0].isIntersecting && hasMore) {
@@ -81,30 +91,43 @@ const Home = () => {
     [loading, hasMore]
   );
 
+  if (videosError) return <div>Failed to load videos</div>;
+  if (subscriptionsError) return <div>Failed to load subscriptions</div>;
+
   return (
     <div className="w-full relative mt-16 flex md:flex-row lg:flex-row">
       <div className="hidden md:flex">
-        <LeftBar subscribedChannels={subscriptions} />
+        <Suspense fallback={<SkeletonDemo />}>
+          <LeftBar subscribedChannels={subscriptions} />
+        </Suspense>
       </div>
-
       <div className="flex-1 grid-container gap-4 p-4">
         {trendingVideos.length > 0
-          ? trendingVideos.map((trendingVideo, index) => (
-              <div
-                ref={
-                  index === trendingVideos.length - 1
-                    ? lastVideoElementRef
-                    : undefined
-                }
-                key={trendingVideo.id}
-              >
-                <VideoCard
-                  video={trendingVideo}
-                  channel={trendingVideo.channel}
-                  channelAvatar={trendingVideo.channel.imageSrc}
-                />
-              </div>
-            ))
+          ? trendingVideos.map((trendingVideo, index) => {
+              if (trendingVideos.length === index + 1) {
+                return (
+                  <div ref={lastVideoElementRef} key={trendingVideo.id}>
+                    <Suspense fallback={<SkeletonCard />}>
+                      <VideoCard
+                        video={trendingVideo}
+                        channel={trendingVideo.channel}
+                        channelAvatar={trendingVideo.channel.imageSrc}
+                      />
+                    </Suspense>
+                  </div>
+                );
+              } else {
+                return (
+                  <Suspense fallback={<SkeletonCard />} key={trendingVideo.id}>
+                    <VideoCard
+                      video={trendingVideo}
+                      channel={trendingVideo.channel}
+                      channelAvatar={trendingVideo.channel.imageSrc}
+                    />
+                  </Suspense>
+                );
+              }
+            })
           : !loading && "No Videos"}
         {loading && <SkeletonCard />}
       </div>
